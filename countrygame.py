00e7efa5ab4c316c24e22f44273410c06a82f98a
aarushiimathur <148770634+aarushiimathur@users.noreply.github.com>
2024-06-15 13:36:45 +0530

    Add files via upload

--- countrygame.py
+++ countrygame.py
@@ -3,7 +3,7 @@ import random
 
 def openfile():
     rows = []
-    with open("country-list.csv", 'r') as file:
+    with open("countrdb1.csv", 'r') as file:
         # This database has been collected from GIT itself and is not owned by me.
         csvreader = csv.reader(file)
         header = next(csvreader)
@@ -28,6 +28,26 @@ def countrycapitals(rows):
                 break
         if found == 0:
             print("Country is not found on the list.")
+        if __name__ == "__main__":
+            main()
+
+def knowcapitals(rows):
+    while True:
+        place = input("Enter the capital (type 'exit' to stop): ").strip()
+        if place.lower() == 'exit':
+            break
+        found = 0
+        for i in rows:
+            country = i[0]
+            capital = i[1]
+            if place.lower() == capital.lower():
+                print("The capital is:", country)
+                found = 1
+                break
+        if found == 0:
+            print("City is not found on the list.")
+        if __name__ == "__main__":
+            main()
 
 def countrygame(rows):
     rrow = random.choice(rows)
@@ -39,12 +59,26 @@ def countrygame(rows):
         print("Correct!!")
     else:
         print("Wrong!! The correct answer is:", rcapital)
-        print("You can know about more countries and their capitals too")
-        countrycapitals(rows)
+    if __name__ == "__main__":
+        main()
+
+def capitals(rows):
+    rrow = random.choice(rows)
+    rcountry = rrow[0]
+    rcapital = rrow[1]
+    answer = input(f"Which country is {rcapital} the capital of? ").strip()
+
+    if answer.lower() == rcountry.lower():
+        print("Correct!!")
+    else:
+        print("Wrong!! The correct answer is:", rcountry)
+    if __name__ == "__main__":
+        main()
+        
 
 def main():
     print("Welcome!!!")
-    choice = input("Would you like to \n 1. Country Game \n 2. Know about countries \n")
+    choice = input("Would you like to \n 1. Country Game \n 2. Capital game\n 3. Know about countries\n 4. Know about the Capitals\n 5. Exit\n")
 
     rows = openfile()
 
@@ -52,10 +86,19 @@ def main():
         print("You have decided to play the Country game.")
         countrygame(rows)
     elif choice == '2':
+        print("You have decided to play the City game.")
+        capitals(rows)
+    elif choice == '3':
         print("Let's know about the capital of countries.")
         countrycapitals(rows)
+    elif choice == '4':
+        print("Lets find out which country the capital belongs to .")
+        knowcapitals(rows)
+    elif choice == '5':
+        print("Thank you for playing")
+        
     else:
-        print("Invalid choice. Please select 1 or 2.")
+        print("Invalid choice. Please select from 1 to 4.")
 
 if __name__ == "__main__":
     main()
